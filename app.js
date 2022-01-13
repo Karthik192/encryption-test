83b3c9c752f485d8355f52ca21769dd2da3bf8d0
Karthik Kirito <devakarthik03102001ece@gmail.com>
2022-01-13 08:48:25 +0530

    Bcrypt

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const sha1 = require('sha1');
+// can use md5 too
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
 
 const app = express();
 
-console.log(process.env.SECRET);
+// console.log(sha1("test4"));
 
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -20,12 +24,37 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// Caeser cipher
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('user', userSchema);
 
 app.get('/', function(req, res){
     res.render("home")
+});
+
+
+// Register route
+app.route('/register')
+
+.get(function(req, res){
+    res.render("register")
+})
+
+.post(function(req, res){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(!err) {
+                res.render('secrets');
+            } else {
+                console.log(err);
+            }
+        });
+    });
 });
 
 
@@ -45,39 +74,15 @@ app.route('/login')
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result){
+                    if(result === true) {
+                        res.render('secrets');
+                    }
+                });
             }
         }
     });
 });
-
-// Register route
-app.route('/register')
-
-.get(function(req, res){
-    res.render("register")
-})
-
-.post(function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-
-    newUser.save(function(err){
-        if(!err) {
-            res.render('secrets');
-        } else {
-            console.log(err);
-        }
-    });
-});
-
-
-
-
 
 
 
--- readme.txt
+++ readme.txt
@@ -9,9 +9,19 @@ Database encryption
 If hecker gets into app.js he can get the secret string and can use the key
 to decrypt the message.
 
-Level - 3:
 Using environmental variables
 If uploading to remote - gitignore .env file
 If pushed to heroku - you need to access configure variables
 
+Level - 3:
+Hashing
+Hashing done at registering and login and compare hash values
+MD5 hashing is easy for common passwords and so hecker can heck
+So strong - unique and long password
 
+Level - 4:
+Hashing and Salting
+Perfect security with SaltingRounds
+
+Level - 5:
+Cookies and sessions
